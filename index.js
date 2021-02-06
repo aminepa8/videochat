0cc0081f09b19d7a7e1d70826114a7a0ec09fdb9
Amine Moulay <aminepa8@gmail.com>
2021-02-06 15:45:29 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,67 @@
+const express = require("express");
+const socket = require("socket.io");
+const app = express();
+
+
+var server = app.listen(4000,function() {
+    console.log("Server is running");
+});
+
+app.use(express.static("public"));
+
+
+//var io = socket(server);
+const io = require('socket.io')(server, {
+    //resolve Missing Cors 
+    cors: { 
+      origin: '*',
+    }
+  });
+io.on("connection",function(socket) {
+    console.log("User Connected : " + socket.id);
+
+    socket.on("join",function(roomName) {
+        console.log("Room Name : "+roomName);
+        var rooms = io.sockets.adapter.rooms;
+        var room = rooms.get(roomName);
+        if (room == undefined) {
+            socket.join(roomName);
+            socket.emit("created")
+            //console.log("Room Created");
+        } else if(room.size==1){
+            socket.join(roomName);
+            socket.emit("joined");
+            //console.log("Room Joined");
+        }else{
+            socket.emit("full");
+            //console.log("Room Full for now");
+        }
+        console.log(rooms);
+    });
+
+    socket.on('ready', function(roomName) {
+        console.log("ready");
+        socket.broadcast.to(roomName).emit("ready");
+    });
+
+    socket.on('candidate', function(candidate,roomName) {
+        console.log("candidate");
+        console.log(candidate);
+        socket.broadcast.to(roomName).emit("candidate",candidate);
+    });
+
+
+    socket.on('offer', function(offer,roomName) {
+
+        console.log("offer");
+        console.log(offer);
+        socket.broadcast.to(roomName).emit("offer",offer);
+    });
+
+    socket.on('answer', function(answer,roomName) {
+        console.log("answer");
+        socket.broadcast.to(roomName).emit("answer",answer);
+    });
+});
+
+
---
+++ public/chat.js
@@ -0,0 +1,139 @@
+var socket = io.connect("http://localhost:4000");
+
+var divVideoChatLobby = document.getElementById("video-chat-lobby");
+var divVideoChat = document.getElementById("video-chat-room");
+var joinButton = document.getElementById("join");
+var userVideo = document.getElementById("user-video");
+var peerVideo = document.getElementById("peer-video");
+var roomInput = document.getElementById("roomName");
+var roomName = roomInput.value;
+var creator = false;
+var rtcPeerConnection;
+var userStream;
+var iceServers = {
+    iceServers:[
+        {urls : "stun:stun.services.mozilla.com"},
+        {urls : "stun:stun1.l.google.com:19302"},
+    ],
+};
+joinButton.addEventListener("click", function () {
+  if (roomInput.value == "") {
+    alert("Please enter a room name");
+  } else {
+    socket.emit("join",roomName);
+  }
+});
+
+
+socket.on("created",function() {
+    creator = true;
+    navigator.mediaDevices
+      .getUserMedia({audio:true,video : true,})
+      .then(function (stream) {
+        userStream = stream;
+          divVideoChatLobby.style= "display:none";
+        userVideo.srcObject = stream;
+        userVideo.onloadedmetadata = function(e) {
+            userVideo.play();
+        };
+      })
+      .catch(function (err) {
+        /* handle the error */
+        alert("Couldn't access User Media");
+      });
+
+});
+
+socket.on("joined",function() {
+    creator = false;
+    navigator.mediaDevices
+      .getUserMedia({audio:true,video : true,})
+      .then(function (stream) {
+        userStream = stream;
+          divVideoChatLobby.style= "display:none";
+        userVideo.srcObject = stream;
+        userVideo.onloadedmetadata = function(e) {
+            userVideo.play();
+        };
+        socket.emit("ready",roomName);
+      })
+      .catch(function (err) {
+        /* handle the error */
+        alert("Couldn't access User Media");
+      });
+});
+
+socket.on("full",function() {
+    alert("Room is full , can't joined");
+});
+
+
+socket.on("ready",function() {
+    if (creator) {
+        rtcPeerConnection = new RTCPeerConnection(iceServers);
+        rtcPeerConnection.onicecandidate = OnIceCandidateFunction;
+        rtcPeerConnection.ontrack = OnTrackFunction;
+        rtcPeerConnection.addTrack(userStream.getTracks()[0],userStream); //videoTrack
+        rtcPeerConnection.addTrack(userStream.getTracks()[1],userStream);//audioTrack
+        rtcPeerConnection.createOffer(function(offer){
+            rtcPeerConnection.setLocalDescription(offer); 
+            console.log("Offer " +offer);
+            socket.emit("offer",offer,roomName);
+        },function(error) {
+            console.log(error);
+        });
+    }
+});
+
+
+
+socket.on("candidate",function(candidate) {
+    var iceCandidate = new RTCIceCandidate(
+        {
+        candidate:candidate.candidate,
+        sdpMid:candidate.sdpMid,
+        sdpMLineIndex: candidate.sdpMLineIndex,
+        }
+    );
+    rtcPeerConnection.addIceCandidate(iceCandidate);
+});
+
+
+
+socket.on("offer",function(offer) {
+    if (!creator) {
+        rtcPeerConnection = new RTCPeerConnection(iceServers);
+        rtcPeerConnection.onicecandidate = OnIceCandidateFunction;
+        rtcPeerConnection.ontrack = OnTrackFunction;
+        rtcPeerConnection.addTrack(userStream.getTracks()[0],userStream); //videoTrack
+        rtcPeerConnection.addTrack(userStream.getTracks()[1],userStream);//audioTrack
+        rtcPeerConnection.setRemoteDescription(offer);
+        rtcPeerConnection.createAnswer(
+            function(answer){
+                rtcPeerConnection.setLocalDescription(answer);     
+                socket.emit("answer",answer,roomName);
+            },
+        function(error) {
+            console.log(error);
+        });
+    }
+});
+
+socket.on("answer",function(answer) {
+    rtcPeerConnection.setRemoteDescription(answer);
+    
+});
+
+
+function OnIceCandidateFunction(event) {
+    if (event.candidate) {
+        socket.emit("candidate",event.candidate,roomName);
+    }
+}
+
+function OnTrackFunction(event) {
+    peerVideo.srcObject = event.streams[0];
+    peerVideo.onloadedmetadata = function(e) {
+        peerVideo.play();
+    };
+}
